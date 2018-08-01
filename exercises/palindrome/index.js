c698204c474c6f6c452ae0dba91838a7a7e45174
phreemason <mr.omarmason@gmail.com>
2018-07-31 23:43:05 -0400

    solutions for palindrome and reversestring

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let newString = str.split('').reverse().join('')
+  return str === newString
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,10 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((result, strChr) => {
+    return strChr + result
+  }, '')
+}
 
 module.exports = reverse;
